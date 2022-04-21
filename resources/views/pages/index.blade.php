9df975162bab4d679ce7b20a9f93afad504323b5
daba clovis <dabaclovis@yahoo.com>
2022-04-20 21:33:56 -0400

    all content

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="jumbotron">
+        <h1 class="display-3">Welcome to my website</h1>
+        <p class="lead">Jumbo helper text</p>
+        <hr class="my-2">
+        <p>read more here</p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+        </p>
+    </div>
+@endsection
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>All posts</h1>
+@endsection
